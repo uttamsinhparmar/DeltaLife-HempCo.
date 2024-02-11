91a83ba104e6dd96d87f9b1926511929139d9943
uttamsinhparmar <155422589+uttamsinhparmar@users.noreply.github.com>
2024-02-11 09:57:38 +0530

    My Task

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+    // Youtube Video Modal
+    $('.play-button').click(function(e){
+        var iframeEl = $('<iframe>', { src: $(this).data('url') });
+        $('.youtubevideo').attr('src', $(this).data('url'));
+    });
+    $('.close-video').click(function(e){
+        $('.youtubevideo').attr('src', '');
+    });
+    $(document).on('hidden.bs.modal','#myModal', function () {
+        $('.youtubevideo').attr('src', '');
+    });
+
+});
